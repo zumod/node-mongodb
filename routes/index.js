dd2132adf92a76684f613947edd62049e30f335b
Sumod Sivadas <contactsumod@gmail.com>
2020-09-14 12:44:53 +0530

    Added comments

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient; //Importing MongoClient module
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/submit', (req,res) => {
+router.post('/submit', (req,res) => { //Post method for handling data
   console.log(req.body)
-  MongoClient.connect('mongodb://localhost:27017',(err,client) => {
-    if (err) throw err
-    client.db('Node').collection('users').insertOne(req.body)
+  MongoClient.connect('mongodb://localhost:27017',(err,client) => { //Connecting mongo db
+    if (err) throw err  // Shows error if db not connected
+    client.db('Node').collection('users').insertOne(req.body) //Callback function
   })
   res.send('User registered successfully')
 })
 
-module.exports = router;
+module.exports = router;  //Exports router module for app.js file
